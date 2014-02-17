8cd4121c3b0953f91cc461d1d9792fe8a6cc3b19
Tim Ekl <lithium3141@gmail.com>
2014-02-17 10:41:37 -0800

    Initial Commit

---
+++ CoreDataCountTest/TEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAppDelegate.h
+//  CoreDataCountTest
+//
+//  Created by Tim Ekl on 2/17/14.
+//  Copyright (c) 2014 Tim Ekl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreDataCountTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataCountTest
+//
+//  Created by Tim Ekl on 2/17/14.
+//  Copyright (c) 2014 Tim Ekl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TEAppDelegate class]));
+    }
+}
